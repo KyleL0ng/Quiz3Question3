1451d9862e43570c7baa1d7612eec46e2ba6aa58
Kyle Long <kjlong@cpp.edu>
2022-10-23 13:50:41 -0700

    Initial commit with layout

---
+++ NumberGuessing.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    //print menu and get user's choice
+
+    //If 1: play game
+    //  pick number
+    //  get guess
+    //  high/low
+    //  repeat until successful
+
+    //If 2: change max number
+
+    //If 3: end
+
+    return EXIT_SUCCESS;
+}
